b6de80015a5b83e655b2080b364898785a54031f
Морозов Никита Александрович <n.morozov.2019@stud.nstu.ru>
2022-02-15 10:38:25 +0700

    Initial committ

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RefactoringGuru\Singleton\Conceptual;
+
+/**
+ * Класс Одиночка предоставляет метод `GetInstance`, который ведёт себя как
+ * альтернативный конструктор и позволяет клиентам получать один и тот же
+ * экземпляр класса при каждом вызове.
+ */
+
+class Singleton
+{
+    private static $instances = [];
+    protected function __construct() { }
+    protected function __clone() { }
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize a singleton.");
+    }
+    public static function getInstance(): Singleton
+    {
+        $cls = static::class;
+        if (!isset(self::$instances[$cls])) {
+            self::$instances[$cls] = new static();
+        }
+        return self::$instances[$cls];
+}
+    public function someBusinessLogic()
+    {
+        // ...
+    }
+}
+function clientCode()
+{
+    $s1 = Singleton::getInstance();
+    $s2 = Singleton::getInstance();
+    if ($s1 === $s2) {
+        echo "Singleton works, both variables contain the same instance.";
+    } else {
+        echo "Singleton failed, variables contain different instances.";
+    }
+}
+
+clientCode();
